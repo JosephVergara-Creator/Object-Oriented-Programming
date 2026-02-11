5a276870e8b078cdfda8d449d066e2ab7d0d3f02
Joseph Vergara <josephvergaraecheverri@gmail.com>
2026-02-10 20:33:41 -0500

    Create EjercicioResueltoN4.java

---
+++ EjercicioResueltoN4.java
@@ -0,0 +1,11 @@
+public class EjercicioResueltoN4 {
+
+    public static void main(String[] args) {
+        double edalber, edana, edmama;
+        double edjuan = 9;
+        edalber = Edades.calcular_edalber(edjuan);
+        edana = Edades.calcuar_edana(edjuan);
+        edmama = Edades.calcular_edmama(edjuan, edalber, edana);
+        System.out.println("Las edades son: Alberto = " + edalber + " Juan = " + edjuan + " Ana = " + edana + " Mama = " + edmama);
+    }
+}
